0db0c8566e74e3bc2034e23989dceaafecefa5d2
HAZAMA <kotonechan@live.jp>
2018-08-18 01:58:43 +0900

    remove signings

--- Expresso/ExpressoRuntime/Properties/AssemblyInfo.cs
+++ Expresso/ExpressoRuntime/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.CompilerServices;
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("sgKey.snk")]
+//[assembly: AssemblyKeyFile("sgKey.snk")]
 
--- Expresso/Properties/AssemblyInfo.cs
+++ Expresso/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.*")]
 
-[assembly: AssemblyKeyFile("sgKey.snk")]
+//[assembly: AssemblyKeyFile("sgKey.snk")]
